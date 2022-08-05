7b3b2f80bfc6184cebadd6a022b7b6b159e57d4d
sandrojr <sjr1999@protonmail.com>
2022-08-05 11:58:30 -0300

    Add content view

---
+++ XamarinBottomDrawer/XamarinBottomDrawer/BottomDrawer.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinBottomDrawer
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BottomDrawer : ContentView
+    {
+        public BottomDrawer()
+        {
+            InitializeComponent();
+        }
+    }
+}
